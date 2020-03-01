c0e0364bccf5221f9b32a9fb417c4da435bac4c9
tech-rajesh <ece.rajesh@gmail.com>
2020-03-01 14:27:05 +0530

    bin

---
+++ src/smokeTest/OrangeHRMTest.java
@@ -0,0 +1,22 @@
+package smokeTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class OrangeHRMTest {
+	
+	
+	@Test
+	public void loginTest() {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Automation\\Tools\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		Assert.assertTrue(driver.getTitle().contains("Goog"));
+		
+	}
+
+}
